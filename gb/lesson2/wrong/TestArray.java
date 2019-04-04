fc776be476ab959e17dda16157e144f114b9f1d5
MrCrazyMouse <66mause66@mail.ru>
2019-04-04 22:07:54 +0700

    Добавление гит игнора и всего кода задания

--- gb/lesson2/wrong/TestArray.java
+++ gb/lesson2/wrong/TestArray.java
@@ -3,19 +3,29 @@ package gb.lesson2.wrong;
 import java.util.Random;
 
 public class TestArray {
-    public static void main (String[] args){
-        int[][] arr = new int[4][4];
-        int max, min;
-        Random rnd = new Random();
-        for (int i=0;i < arr.length;i++) {
-            for (int j=0;j < arr[i].length;j++) {
-                arr[i][j]=rnd.nextInt(10) + 1;
+    public static void main (String[] args) {
+        try {
+            int[][] arr = new int[4][4];
+            int max, min;
+            Random rnd = new Random();
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = 0; j < arr[i].length; j++) {
+                    arr[i][j] = rnd.nextInt(10) + 1;
+                }
             }
-        }
-        for (int i=0;i < arr.length;i++,System.out.println()) {
-            for (int j=0;j < arr[i].length;j++) {
-                System.out.print(arr[i][j]+" ");
+            for (int i = 0; i < arr.length; i++, System.out.println()) {
+                for (int j = 0; j < arr[i].length; j++) {
+                    System.out.print(arr[i][j] + " ");
+                }
             }
+        }catch (ArrayIndexOutOfBoundsException ex) {
+            System.out.println("Ошибка " + ex);
+       /* }catch (MyArraySizeException ex) {
+            System.out.println("Ошибка " + ex);
+        }catch (MyArrayDataException  ex) {
+            System.out.println("Ошибка " + ex);
+        }catch (MySizeArrayException   ex) {
+          System.out.println("Ошибка " + ex);*/
         }
     }
 }
